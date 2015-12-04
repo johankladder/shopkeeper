b856f2c79b1b48b70bc77865c42ba0d670174b8c
Johan Kladder <j.kladder@st.hanze.nl>
2015-12-04 15:15:56 +0100

    ADD: Map generator in the subject parser. INC Tests

--- src/main/java/org/shopkeeper/parsers/SubjectParser.java
+++ src/main/java/org/shopkeeper/parsers/SubjectParser.java
@@ -1,17 +1,20 @@
 package org.shopkeeper.parsers;
 
+import org.apache.commons.lang3.StringUtils;
 import org.shopkeeper.subjects.Subject;
 import org.shopkeeper.subjects.categories.Category;
 import org.shopkeeper.subjects.customer.Customer;
 import org.shopkeeper.subjects.items.Item;
+
+import java.util.HashMap;
+import java.util.jar.Pack200;
 
 /**
  * Created by typhooncoaster on 4-12-15.
  */
 public class SubjectParser {
 
-    // Make sure all the fields are stored here
-
+    // Make sure all the fields are stored here when adding a new subject:
     // GENERAL
     public static final String IDNUMBER = "id_number_general";
     public static final String NAME = "name_general";
@@ -29,12 +32,55 @@ public class SubjectParser {
     // CATEGORIES:
     // Only has general fields:
 
-    public static void generateMapForStorage(Integer subjectType) {
-
+    /**
+     * Gets the hashmap with keys according to the subject-type. Will return null if subject-type is not supported
+     */
+    public static HashMap<String, Object> generateMapForStorage(Integer subjectType) {
+        if (subjectType == SubjectTypes.ITEM) {
+            return generateMapItem();
+        } else if (subjectType == SubjectTypes.CATEGORY) {
+            return generateMapCategories();
+        } else if (subjectType == SubjectTypes.CUSTOMER) {
+            return generateMapCustomer();
+        } else {
+            return null;
+        }
     }
 
+    // Map generations:
+    private static HashMap generateMapItem() {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        // Put the fields in it:
+        map.put(IDNUMBER, null);
+        map.put(NAME, null);
+        map.put(ITEM_PRICE, null);
+        return map;
+    }
+
+    private static HashMap generateMapCustomer() {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        // Put the fields in it:
+        map.put(IDNUMBER, null);
+        map.put(NAME, null);
+        map.put(CUSTOMER_PLACE, null);
+        map.put(CUSTOMER_EMAIL, null);
+        map.put(CUSTOMER_ADDRESS, null);
+        map.put(CUSTOMER_PHONE, null);
+        map.put(CUSTOMER_ZIPCODE, null);
+        return map;
+    }
+
+    private static HashMap generateMapCategories() {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        // Put the fields in it:
+        map.put(IDNUMBER, null);
+        map.put(NAME, null);
+        return map;
+    }
+
+
     public static Integer parseTypeFromSubject(Subject subject) {
-        if(subject instanceof Item) {
+        if (subject instanceof Item) {
             return SubjectTypes.ITEM;
         } else if (subject instanceof Category) {
             return SubjectTypes.CATEGORY;
--- src/test/java/org/shopkeeper/parsers/SubjectParserTest.java
+++ src/test/java/org/shopkeeper/parsers/SubjectParserTest.java
@@ -7,6 +7,8 @@ import org.shopkeeper.subjects.categories.Category;
 import org.shopkeeper.subjects.customer.Customer;
 import org.shopkeeper.subjects.items.Item;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 /**
@@ -16,26 +18,49 @@ public class SubjectParserTest {
 
     @Test
     public void testParser() {
-        Item item = new Item(new Long(1111), "Test", 12.0, new DateTime(1,1,1,1,1));
+        Item item = new Item(new Long(1111), "Test", 12.0, new DateTime(1, 1, 1, 1, 1));
         int testcase = SubjectTypes.ITEM;
         int actual = SubjectParser.parseTypeFromSubject(item);
         int not = SubjectTypes.CUSTOMER;
         assertEquals(testcase, actual);
         assertNotEquals(not, actual);
 
-        Customer c = new Customer(new Long(1111), "Test", new DateTime(1,1,1,1,1), null, null, null, null, null);
+        Customer c = new Customer(new Long(1111), "Test", new DateTime(1, 1, 1, 1, 1), null, null, null, null, null);
         testcase = SubjectTypes.CUSTOMER;
         actual = SubjectParser.parseTypeFromSubject(c);
         not = SubjectTypes.ITEM;
         assertEquals(testcase, actual);
         assertNotEquals(not, actual);
 
-        Category ca = new Category(new Long(1111), "Test", new DateTime(1,1,1,1,1));
+        Category ca = new Category(new Long(1111), "Test", new DateTime(1, 1, 1, 1, 1));
         testcase = SubjectTypes.CATEGORY;
         actual = SubjectParser.parseTypeFromSubject(ca);
         not = SubjectTypes.ITEM;
         assertEquals(testcase, actual);
         assertNotEquals(not, actual);
+    }
+
+    @Test
+    public void testHashMapGetter() {
+        HashMap<String, Object> map = SubjectParser.generateMapForStorage(SubjectTypes.ITEM);
+        boolean status;
+        if (map.containsKey(SubjectParser.ITEM_PRICE) && map.containsKey(SubjectParser.NAME) && map.containsKey(SubjectParser.IDNUMBER)
+                && !map.containsKey(SubjectParser.CUSTOMER_ADDRESS)) {
+            status = true;
+        } else {
+            status = false;
+        }
+
+
+        HashMap<String, Object> map2 = SubjectParser.generateMapForStorage(SubjectTypes.CUSTOMER);
+        boolean status2;
+        if (!map2.containsKey(SubjectParser.ITEM_PRICE) && map2.containsKey(SubjectParser.CUSTOMER_ADDRESS) && map2.containsKey(SubjectParser.CUSTOMER_EMAIL)
+                && map2.containsKey(SubjectParser.NAME)) {
+            status2 = true;
+        } else {
+            status2 = false;
+        }
+        assertTrue(status2);
     }
 
 }
