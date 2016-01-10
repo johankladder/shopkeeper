80b770a5f5fab55d6ee5526d5598f701734f76f4
Johan Kladder <j.kladder@st.hanze.nl>
2016-01-10 12:19:46 +0100

    DOC

--- src/main/java/org/shopkeeper/preferences/PreferenceLoader.java
+++ src/main/java/org/shopkeeper/preferences/PreferenceLoader.java
@@ -1,9 +1,14 @@
 package org.shopkeeper.preferences;
+
 import org.shopkeeper.preloader.Preloader;
+
 import java.util.prefs.Preferences;
 
 /**
- * Created by johankladder on 12/23/15.
+ * This class loads all the given preferences. It will also look for default preferences values. This class is made to be
+ * invoked in the preloader class and will not fully work if it is invoked at a later point. This causes that the user needs
+ * to, if it want to see the changes in the newly added preference, first needs to restart the application so that the preloader
+ * is invoking this runnable again.
  */
 public class PreferenceLoader implements Runnable {
 
